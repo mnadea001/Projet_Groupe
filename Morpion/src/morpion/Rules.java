c2e0e7240fcb8dd7edfef3ee0829d53367cf14e5
Arnaud Boyer <r.boyer895@laposte.net>
2022-12-18 03:36:12 +0100

    push intermédiaire checkLine finit checkColumns en cours

--- Morpion/src/morpion/Rules.java
+++ Morpion/src/morpion/Rules.java
@@ -1,8 +1,29 @@
 package morpion;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Rules {
+	private Boolean checkLine(Case[] arr) {
+		return arr[0].getSymbol() == arr[1].getSymbol() && arr[0].getSymbol() == arr[2].getSymbol() && arr[1].getSymbol() == arr[2].getSymbol();
+	}
+	
+	private Boolean checkColumns(ArrayList<Case> col) {
+		return null;
+	}
 
 	public Boolean checkVictory(Board board) {
-		return false;
+		ArrayList<Case> col1 = new ArrayList<Case>();
+		ArrayList<Case> col2 = new ArrayList<Case>();
+		ArrayList<Case> col3 = new ArrayList<Case>();
+		ArrayList<ArrayList<Case>> columns = new ArrayList<ArrayList<Case>>(Arrays.asList(col1, col2, col3));
+		for(Case[] line : board.lines) {
+			Boolean checkLine = checkLine(line);
+			if(!checkLine) return false;
+			for(int i=0; i<3; i++) {
+				columns.get(i).add(line[i]);
+			}
+		}
+		return true;
 	}
 }
